45bbad01da1de6350db957ae097cca97b9eeb948
Samhith Nsk <samhithnsk@Samhiths-MacBook-Pro.local>
2025-01-26 12:08:43 -0800

    Recovered student-fe folder

---
+++ student-fe/src/App.js
@@ -0,0 +1,15 @@
+import Appbar from "../src/components/Appbar.jsx"
+import Student from "./components/Student.jsx";
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Appbar />
+      <Student />
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
